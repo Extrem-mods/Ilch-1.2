0fe39e315ca97283191d498ba0a5ef1dc1ff3e59
finke <Surf-finke@gmx.de>
2013-07-01 01:06:07 +0200

    ﻿PwChrypt Klasse überarbeitet. - Neue Funktionen: * public static function getRndNumber($min = 0, $max = 255, &$quality = 2) - Erzeugen eienr Zufallszahl, wahlweise Pseudozufalszahl oder echte Zufallszahl * private static function generatePool($chars = self::ALPHA_NUM) - Generiert einen Pool für die Erzeugung von Zufälligen Zeichenketten * public static function getUuid($size = 15, $chars = self::ALPHA_NUM) - Erstellt eine zufällige Zeichenkette die garantiert eineindeutig ist Änderungen an der API: - Zufällige Zeichenketten enthalten jetzt standartmäig Alphanumerische Zeichen, nicht merh nur Buchstaben

--- include/includes/class/pwcrypt.php
+++ include/includes/class/pwcrypt.php
@@ -8,7 +8,7 @@ defined('main') or die('no direct access');
 
 /**
  * PwCrypt
- * 
+ *
  * Achtung: beim Übertragen von mit 2a erzeugten Passwörtern auf einen anderen PC/Server,
  * dort kann es u.U. Passieren, dass eine Authentifikation nicht mehr möglich ist,
  * da 2a auf einigen System fehlerhafte Ergebnisse liefert.
@@ -69,16 +69,99 @@ class PwCrypt
     }
 
     /**
-     * Erstellt eine zufällige Zeichenkette
+    * Liefert eine Zufallszahl zwichen gewählten Grenzen
+    * 
+    *Liefert Eine Zufallszahl zwichen $min und $max. Der Parameter $quality bestimmt die Qualität der verwendeten Zufallszahlen.
+    * 1: Die Zufallszahlen werden durch einen Pseudozufallsgenerator erzeugt
+    * 2: Die Zahlen werden duch einen Pseudozufallszahlen Generator erzeugt, welcher regelmäßig mit echten Zufallszahlen neu initiiert wird
+    * 3: Echte Zufallszahlen. Diese sind allerdings nur begrenztz verfügbar und blockieren, wenn die verfügbare entropie erschöpft ist.
+    * Es wird empfohlen die my_crypt erweiterung zu installieren, ohen diese stehet unter Windows nur Qualitätsstufe 1 zur Verfügung.
+    * 
+    * @author finke <Surf-finke@gmx.de>
+    * @param $min Der niedrigste zurückzugebende Wert (Vorgabe: 0) 
+    * @param $max Der nhöchste zurückzugebende Wert (Vorgabe: 255, max ) 
+    * @param $quality Angabe über die Qualität der Zufallszahlen. (1-3, Vorgabe 2)
+    * @return int Zufahlszahl
+    */
+    public static function getRndNumber($min = 0, $max = 255, &$quality = 2){
+        $quality = intval($quality);
+        if($min > $max){
+            return false;
+        }
+        $bytes = (int)ceil(log($max, 2)/8);
+        if($bytes > 4){
+            return false;
+        }        
+        $n = $max - $min + 1;
+        $m = (float)pow(2,8*$bytes);
+
+        if($n > $m){
+            return false;
+        }
+
+        $k = $n%$m;
+
+        do{
+            $number = '';
+            if(function_exists('mcrypt_create_iv')){
+                if ((version_compare(PHP_VERSION, '5.3.0', '<')
+                && strtoupper(substr(PHP_OS, 0, 3)) === 'WIN')
+                || $quality <=1) {
+                    $number = mcrypt_create_iv($bytes,  MCRYPT_RAND);
+                    $quality =1;
+                }elseif($quality >=3){
+                    $number = mcrypt_create_iv($bytes,  MCRYPT_DEV_RANDOM );
+                    $quality = 3;
+                }elseif($quality == 2){
+                    $number = mcrypt_create_iv($bytes,   MCRYPT_DEV_URANDOM);
+                }
+            }else{
+                if($quality >=3){
+                    $quality = 3;
+                    $fp = @fopen('/dev/random','rb');
+                    if ($fp !== FALSE) {
+                        $number = fread($fp, $bytes);
+                    }
+                    if(strlen($number) != $bytes){
+                        $quality = 2;
+                    }                    
+                }
+                if($quality == 2){
+                    $fp = @fopen('/dev/urandom','rb');
+                    if ($fp !== FALSE) {
+                        $number = fread($fp, $bytes);
+                    }
+                    if(strlen($number) != $bytes){
+                        $quality = 1;
+                    }
+                }
+                if($quality <=1){
+                    $number = '';
+                    for($i = 0; $i < $bytes;$i++){
+                        $number .= pack('C', mt_rand(0,255));
+                    }
+                    $quality = 1;
+                }
+            }
+            
+            $number = str_pad($number, 4, chr(0), STR_PAD_LEFT);
+            $number = unpack('N', $number);
+            $number = $number[1];
+        }while($number >= ($m - $k));
+        return $min + $number % $k;
+    }
+
+
+    /**
+     * Generiert einen String, welcher die Grundlage für das erzeugen von Zufallszahlen bildet
      *
-     * @param integer $size Länge der Zeichenkette
-     * @param integer $chars Angabe welche Zeichen für die Zeichenkette verwendet werden
+     * @param integer $chars Angabe welche Zeichen verwendet werden
      * @return string
      */
-    public static function getRndString($size = 20, $chars = self::LETTERS)
-    {
+    private static function generatePool($chars = self::ALPHA_NUM){
+        $pool = '';
         if ($chars & self::LETTERS) {
-            $pool = 'abcdefghijklmnopqrstuvwxyz';
+            $pool .= 'abcdefghijklmnopqrstuvwxyz';
             $pool .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
         }
 
@@ -95,15 +178,58 @@ class PwCrypt
         if ($chars & self::SPECIAL_CHARACTERS) {
             $pool .= '!#$%&()*+,/:;=?@[]';
         }
+        return $pool;
+    }
 
-        $pool = str_shuffle($pool);
+    /**
+     * Erstellt eine zufällige Zeichenkette
+     *
+     * @param integer $size Länge der Zeichenkette
+     * @param integer $chars Angabe welche Zeichen für die Zeichenkette verwendet werden
+     * @return string
+     */
+    public static function getRndString($size = 20, $chars = self::ALPHA_NUM, &$quality = 2)
+    {
+        $pool = str_shuffle(self::generatePool($chars));
         $pool_size = strlen($pool);
         $string = '';
         for ($i = 0; $i < $size; $i++) {
             //TODO: Zufallszahlen aus /dev/random bzw /dev/urandom wenn verfügbar
-            $string .= $pool[mt_rand(0, $pool_size - 1)];
+            $string .= $pool[self::getRndNumber(0, $pool_size - 1, $quality)];
         }
         return $string;
+    }
+
+    /**
+     * Erstellt eine zufällige Zeichenkette die garantiert eineindeutig ist
+     * Der erste Teil, je nach Verwendeten Zeichen 8-10 Stellen, werden dabei in abhänigkeit der Systemzeit berechet, der rest wird zufällig aufgefüllt.
+     *
+     * @param integer $size Länge der Zeichenkette
+     * @param integer $chars Angabe welche Zeichen für die Zeichenkette verwendet werden
+     * @return string
+     */
+    public static function getUuid($size = 15, $chars = self::ALPHA_NUM){
+
+        $pool = self::generatePool($chars);
+        $pool_size = strlen($pool);
+        $string_bin = base_convert(str_replace(array(' ', '.'), '', microtime()), 10,2);
+
+        if(strlen($string_bin)%7 != 0){
+            $string_bin = str_pad($string_bin, (strlen($string_bin) + (7-strlen($string_bin)%7)),'0', STR_PAD_LEFT);
+        }
+
+        $array = str_split($string_bin, 7);
+        $string = '';
+
+        foreach($array as $i){
+            $string .=   $pool[(base_convert($i, 2, 10))%$pool_size];
+        }
+
+        if($size < strlen($string)){
+            return substr($string, 0, $size);
+        }else{
+            return $string . self::getRndString($size - strlen($string), $chars);
+        }
     }
 
     /**
@@ -141,7 +267,7 @@ class PwCrypt
         switch ($this->hashAlgorithm) {
             case self::SHA512:
             case self::SHA256:
-                $salt = (empty($salt) ? self::getRndString(16, self::LETTERS | self::NUMBERS) : $salt);
+                $salt = (empty($salt) ? self::getRndString(16, self::ALPHA_NUM) : $salt);
                 if ($rounds < 1000 || $rounds > 999999999) {
                     $rounds = mt_rand(2000, 10000);
                 }
@@ -150,7 +276,7 @@ class PwCrypt
             case self::BLOWFISH:
             case self::BLOWFISH_OLD:
             case self::BLOWFISH_FALSE:
-                $salt = (empty($salt) ? self::getRndString(22, self::LETTERS | self::NUMBERS) : $salt);
+                $salt = (empty($salt) ? self::getRndString(22, self::ALPHA_NUM) : $salt);
                 if ($rounds < 4 || $rounds > 31) {
                     $rounds = mt_rand(6, 10);
                 }
@@ -158,7 +284,7 @@ class PwCrypt
                 $salt_string = '$' . $this->hashAlgorithm . '$' . $rounds . '$' . $salt . '$';
                 break;
             case self::MD5:
-                $salt = (empty($salt) ? self::getRndString(12, self::LETTERS | self::NUMBERS) : $salt);
+                $salt = (empty($salt) ? self::getRndString(12, self::ALPHA_NUM) : $salt);
                 $salt_string = '$' . $this->hashAlgorithm . '$' . $salt . '$';
                 break;
             default:
